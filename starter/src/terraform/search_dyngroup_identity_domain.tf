1c15737d5a317780453acf326c5b4440fe59b0f3
Marc Gueury <marc.gueury@oracle.com>
2025-04-03 22:12:57 +0200

    v1

--- starter/src/terraform/search_dyngroup_identity_domain.tf
+++ starter/src/terraform/search_dyngroup_identity_domain.tf
@@ -11,40 +11,17 @@ locals {
   idcs_url = (var.idcs_url!="")?var.idcs_url:data.oci_identity_domains.starter_domains.domains[0].url
 }
 
-resource "oci_identity_domains_dynamic_resource_group" "search-fn-dyngroup" {
-    #Required
-    provider       = oci.home    
-    display_name = "${var.prefix}-fn-dyngroup"
-    idcs_endpoint = local.idcs_url
-    matching_rule = "ALL {resource.type = 'fnfunc', resource.compartment.id = '${local.lz_app_cmp_ocid}'}"
-    schemas = ["urn:ietf:params:scim:schemas:oracle:idcs:DynamicResourceGroup"]
-}
-
-resource "oci_identity_domains_dynamic_resource_group" "search-bastion-dyngroup" {
-    #Required
-    provider       = oci.home    
-    display_name = "${var.prefix}-bastion-dyngroup"
-    idcs_endpoint = local.idcs_url
-    matching_rule = "ALL {instance.id = '${oci_core_instance.starter_bastion.id}'}"
-    schemas = ["urn:ietf:params:scim:schemas:oracle:idcs:DynamicResourceGroup"]
-}
-
-resource "time_sleep" "wait_30_seconds" {
-  depends_on = [ oci_identity_domains_dynamic_resource_group.search-bastion-dyngroup, oci_identity_domains_dynamic_resource_group.search-fn-dyngroup ]
-  create_duration = "30s"
-}
 
 resource "oci_identity_policy" "starter_search_policy" {
     provider       = oci.home    
-    depends_on     = [ time_sleep.wait_30_seconds ]
     name           = "${var.prefix}-policy"
     description    = "${var.prefix} policy"
     compartment_id = local.lz_serv_cmp_ocid
 
     statements = [
-        "Allow dynamic-group ${var.prefix}-fn-dyngroup to manage objects in compartment id ${local.lz_serv_cmp_ocid}",
-        "Allow dynamic-group ${var.prefix}-bastion-dyngroup to manage all-resources in compartment id ${local.lz_serv_cmp_ocid}",
-        "Allow dynamic-group ${var.prefix}-bastion-dyngroup to manage stream-family in compartment id ${local.lz_serv_cmp_ocid}"
+        "allow any-user to manage objects in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${oci_functions_function.starter_fn_function.id}'",
+        "allow any-user to manage all-resources in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${oci_core_instance.starter_bastion.id}'",
+        "allow any-user to manage stream-family in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${oci_core_instance.starter_bastion.id}'"
         # "Allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-fn-dyngroup to manage objects in compartment id ${var.compartment_ocid}",
         # "Allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-bastion-dyngroup to manage all-resources in compartment id ${var.compartment_ocid}",
         # "Allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-bastion-dyngroup to manage stream-family in compartment id ${var.compartment_ocid}"
