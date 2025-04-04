7e7b28ffb6b6d8a13de5cbb20a25ce3b8cfd200e
Marc Gueury <marc.gueury@oracle.com>
2025-04-04 08:37:34 +0200

    v1

--- starter/src/terraform/psql.tf
+++ starter/src/terraform/psql.tf
@@ -1,4 +1,29 @@
 // ------------------------ PostgreSQL -----------------------------   
+resource "oci_psql_configuration" "starter_psql_configuration" {
+    #Required
+    compartment_id = local.lz_db_cmp_ocid
+    db_configuration_overrides {
+        #Required
+    #        #Required
+    #        config_key = var.configuration_db_configuration_overrides_items_config_key
+    #        overriden_config_value = var.configuration_db_configuration_overrides_items_overriden_config_value
+    #    }
+        items {
+          config_key = "oci.admin_enabled_extensions"
+          overriden_config_value = "vector"
+        }    
+    }
+    db_version = "15"
+    display_name = "${var.prefix}-psql-config"
+    shape = "VM.Standard.E4.Flex"
+
+    #Optional
+    freeform_tags = local.freeform_tags
+    is_flexible = true
+}
+
+
+
 resource "oci_psql_db_system" "starter_psql" {
   compartment_id      = local.lz_db_cmp_ocid
   instance_count = "1"
@@ -11,6 +36,8 @@ resource "oci_psql_db_system" "starter_psql" {
     subnet_id = data.oci_core_subnet.starter_db_subnet.id
   }
   shape = "PostgreSQL.VM.Standard.E4.Flex.2.32GB"
+  config_id = oci_psql_configuration.starter_psql_configuration.id
+  
   storage_details {
     # is_regionally_durable = false # For Frankfurt
     is_regionally_durable = false
@@ -40,26 +67,3 @@ locals {
     jdbc_url = format("jdbc:postgresql://%s:%s/postgres", local.db_host, local.db_port )
 }  
 
-
-resource "oci_psql_configuration" "starter_psql_configuration" {
-    #Required
-    compartment_id = local.lz_db_cmp_ocid
-    db_configuration_overrides {
-        #Required
-    #        #Required
-    #        config_key = var.configuration_db_configuration_overrides_items_config_key
-    #        overriden_config_value = var.configuration_db_configuration_overrides_items_overriden_config_value
-    #    }
-        items {
-          config_key = "oci.admin_enabled_extensions",
-          overriden_config_value = "vector"
-        }    
-    }
-    db_version = "15"
-    display_name = "${var.prefix}-psql-config"
-    shape = "VM.Standard.E4.Flex"
-
-    #Optional
-    freeform_tags = local.freeform_tags
-    is_flexible = true
-}
